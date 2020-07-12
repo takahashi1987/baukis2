cbc8d344755b8083abe27ab386427e267b5fd980
takahashi1987 <takahashi.y1022@gmail.com>
2020-07-12 11:42:40 +0900

    ルーティング リファクタリング

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,4 +1,7 @@
 class Admin::TopController < Admin::Base
   def index
+    if current_administrator
+      render action: "dashboard"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-  namespace :staff do
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: %i(create destroy)
+      resource :account, except: %i(new create destroy)
+    end
   end
 
-  namespace :admin do
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+  constraints host: config[:admin][:host] do
+    namespace :admin, path: config[:admin][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: %i(create destroy)
+      resources :staff_members
+    end
   end
 
   namespace :customer do

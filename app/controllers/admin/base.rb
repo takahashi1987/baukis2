e7f7f459b3684b1f216ccaa1c6e114a4cf3d994f
takahashi1987 <takahashi.y1022@gmail.com>
2020-07-22 14:34:33 +0900

    admin アクセス制御

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,11 +1,19 @@
 class Admin::Base < ApplicationController
   helper_method :current_administrator
+  before_action :authorize
 
   private
 
   def current_administrator
     if session[:administrator_id]
       @current_administrator ||= Administrator.find(session[:administrator_id])
+    end
+  end
+
+  def authorize
+    unless current_administrator
+      flash.alert = "管理者としてログインしてください。"
+      redirect_to :admin_login
     end
   end
 end

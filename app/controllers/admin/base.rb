cfa097a277d05358afcdb8081b4316a8af86d1c3
takahashi1987 <takahashi.y1022@gmail.com>
2020-07-28 16:10:02 +0900

    Admin セッションタイムアウト機能追加

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,6 +1,6 @@
 class Admin::Base < ApplicationController
   helper_method :current_administrator
-  before_action :authorize, :check_account
+  before_action :authorize, :check_account, :check_timeout
 
   private
 
@@ -22,6 +22,20 @@ class Admin::Base < ApplicationController
       session.delete(:administrator_id)
       flash.alert = "アカウントが無効になりました。"
       redirect_to :admin_root
+    end
+  end
+
+  TIMEOUT = 60.minutes
+
+  def check_timeout
+    if current_administrator
+      if session[:last_access_time] >= TIMEOUT.ago
+        session[:last_access_time] = Time.current
+      else
+        session.delete(:administrator_id)
+        flash.alert = "セッションがタイムアウトしました。"
+        redirect_to :admin_login
+      end
     end
   end
 end

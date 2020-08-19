cfe941169f98cdabdc8e0d1897bca459f75c1a9b
takahashi1987 <takahashi.y1022@gmail.com>
2020-08-19 11:56:15 +0900

    HtmlBuilder 作成

--- app/presenters/model_presenter.rb
+++ app/presenters/model_presenter.rb
@@ -1,6 +1,8 @@
 class ModelPresenter
+  include HtmlBuilder
+
   attr_reader :object, :view_context
-  delegate :row, to: :view_context
+  delegate :row, :link_to, to: :view_context
 
   def initialize(object, view_context)
     @object = object

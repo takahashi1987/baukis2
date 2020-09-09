47c9ff467c2f62dfb55bafeed95a089ecbe96a4c
takahashi1987 <takahashi.y1022@gmail.com>
2020-09-09 16:54:43 +0900

    Phone モデル追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,9 @@ class Customer < ApplicationRecord
 
   has_one :home_address, dependent: :destroy, autosave: true
   has_one :work_address, dependent: :destroy, autosave: true
+  has_many :phones, dependent: :destroy
+  has_many :personal_phones, -> { where(address_id: nil).order(:id) },
+    class_name: "Phone", autosave: true
 
   validates :gender, inclusion: { in: %w(male female), allow_black: true }
   validates :birthday, date: { after: Date.new(1900, 1, 1), before: ->(obj) { Date.today }, allow_blank: true }
--- app/presenters/customers/customer_presenter.rb
+++
@@ -1,7 +0,0 @@
-class CustomerPresenter < ModelPresenter
-  delegate :email, to: :object
-
-  def full_name
-    object.family_name + " " + 
-  end
-end

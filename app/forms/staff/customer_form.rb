7266476bf296cb72eb0ae286ad589db897747d34
takahashi1987 <takahashi.y1022@gmail.com>
2020-09-11 11:15:54 +0900

    職員が勤務先電話番号の 登録、更新、削除 出来る機能

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -17,6 +17,9 @@ class Staff::CustomerForm
     (2 - @customer.home_address.phones.size).times do
       @customer.home_address.phones.build
     end
+    (2 - @customer.work_address.phones.size).times do
+      @customer.work_address.phones.build
+    end
   end
 
   def assign_attributes(params = {})
@@ -54,6 +57,17 @@ class Staff::CustomerForm
 
     if inputs_work_address
       customer.work_address.assign_attributes(work_address_params)
+      phones = phone_params(:work_address).fetch(:phones)
+
+      customer.work_address.phones.size.times do |index|
+        attributes = phones[index.to_s]
+
+        if attributes && attributes[:number].present?
+          customer.work_address.phones[index].assign_attributes(attributes)
+        else
+          customer.work_address.phones[index].mark_for_destruction
+        end
+      end
     else
       customer.work_address.mark_for_destruction
     end
@@ -85,7 +99,7 @@ class Staff::CustomerForm
   end
 
   def work_address_params
-    @params.require(:work_address).permit(
+    @params.require(:work_address).except(:phones).permit(
       :postal_code, 
       :prefecture, 
       :city, 

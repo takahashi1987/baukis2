51af7276a395d8564fd04237af9608999030cf60
takahashi1987 <takahashi.y1022@gmail.com>
2020-10-07 11:24:12 +0900

    Revert "ruby バーション修正"
    This reverts commit 2dffac213ed3803719cf39b1758671bffcfd5fb9.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "
 
 set :keep_releases, 5
 
-set :rbenv_ruby, "2.6.6"
+set :rbenv_ruby, "2.6.4"
 
 set :log_level, :debug
 
